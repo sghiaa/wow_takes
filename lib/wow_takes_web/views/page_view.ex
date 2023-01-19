d84ad0c4471543bceaeee37525741b5c5114ff03
Alex Sghia-Hughes <alex@Alexs-MacBook-Pro.local>
2023-01-19 14:58:43 -0500

    add some more takes and topics

--- lib/wow_takes_web/views/page_view.ex
+++ lib/wow_takes_web/views/page_view.ex
@@ -5,23 +5,32 @@ defmodule WowTakesWeb.PageView do
   def tweet() do
     tweet(format())
   end
-  def tweet(:if_loving) do
-    topic = topic_text(topic())
+  def tweet(:if_loving_wrong) do
+    topic = translate(topic())
     "if loving #{topic} is wrong i dont want to love #{topic}"
   end
+  def tweet(:if_loving_right) do
+    "if loving #{translate(topic())} is wrong i dont want to be right"
+  end
   def tweet(:hasnt_been_fun) do
-    "#{topic_text(topic())} hasnt been #{topic_text(subjective_value)} since #{topic_text(expansion())}"
+    "#{translate(spec())} hasnt been #{translate(subjective_value)} since #{translate(expansion())}"
   end
   def tweet(:was_good_actually) do
-    "#{topic_text(system())} was #{topic_text(subjective_value())} actually"
+    "#{translate(system())} was #{translate(subjective_value())} actually"
   end
   def tweet(:i_miss) do
-    "i miss grinding #{topic_text(old_dungeon())}"
+    "i miss grinding #{translate(old_dungeon())}"
   end
   def tweet(:remember_when) do
-    "remember when #{topic_text(topic)} was #{topic_text(subjective_value)}"
+    "remember when #{translate(topic)} was #{translate(subjective_value)}"
   end
   def tweet(:can_we_go_back_to) do
-    "can we go back to #{topic_text(expansion())}"
+    "can we go back to #{translate(expansion())}"
+  end
+  def tweet(:i_used_to) do
+    "i used to play #{translate(spec())} in #{translate(old_dungeon())} but now its all #{translate(spec())} in #{translate(dungeon())}"
+  end
+  def tweet(:buff_nerf) do
+    "honestly blizzard needs to #{translate(buff_nerf())} #{translate(spec())}"
   end
 end
